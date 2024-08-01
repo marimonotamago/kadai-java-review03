02ff041efcd0d3ff6df86d9be3f013dc22b71639
marimonotamago <golupio24@gmail.com>
2024-08-02 04:23:51 +0900

    コードの修正、再提出

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -15,16 +15,15 @@ public class BaseBallTeam {
         this.draw = draw;
     }
 
-    //勝率の計算メソッド　勝ち数/(勝ち数+負け数)
-    public double getRate(int win, int lose) {
-        double rate = this.win / (this.win + this.lose);
+    
+    //勝率の計算メソッド　勝ち数/(勝ち数+負け数)と戻り値
+    public double getRate() {
+        double rate = (double)this.win / (this.win + this.lose);
         return rate;
     }
 
     //成績の出力メソッド
-    public double report(this.name, this.win, this.lose, this.draw, double rate) {
-        System.out.println(this.name +"の2022年の成績は" + win + "勝" + lose + "敗" + draw + "勝率は0."+ rate + "です。");
+    public void report() {
+        System.out.println(this.name +"の2022年の成績は" + win + "勝" + lose + "敗" + draw + "分、" + "勝率は"+ getRate() + "です。");
     }
-
-
- }
+}
